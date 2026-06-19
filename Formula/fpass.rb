eafc4c0ab3d9fb6a4247d7bec4686e465765755b
Higor Henrique Prata Gonçalves <higorhenrique.gon@icloud.com.br>
2026-06-19 03:06:23 -0300

    fix: corrigido hash

--- Formula/fpass.rb
+++ Formula/fpass.rb
@@ -1,8 +1,8 @@
 class Fpass < Formula
   desc "Gerenciador de senhas com interface TUI para KeePassXC (.kdbx)"
   homepage "https://github.com/higorhgon/fpass"
-  url "https://github.com/higorhgon/fpass/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "SKIP"
+  url "https://github.com/higorhgon/fpass/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "ef37b2a15404caae9de6081ca44235112c611daad4075d34a5938aa8725ab14c"
   license "MIT"
 
   depends_on "rust" => :build
